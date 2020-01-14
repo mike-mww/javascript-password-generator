10560d3f34cd348f2a930b8d442ad8c99c803208
Mike Holguin <mholguin@mammothwebworks.com>
2020-01-14 13:31:31 -0800

    Fixed build dist path in Webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,7 +8,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve( __dirname, '../dist' ),
+        path: path.resolve( __dirname, './dist' ),
         filename: '[name].[hash].js'
     },
     module: {
